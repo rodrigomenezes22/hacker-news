9b5b374da1cff14b0bb151c4d9c20f789552f2e3
rodrigomenezes22 <rodrigomenezes22@gmail.com>
2023-02-01 16:23:10 +0100

    Modules

---
+++ src/Components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/menu.css'
+
+function Menu() {
+  return (
+    <div>
+        <h1 className='red'>Menu</h1>
+    </div>
+  )
+}
+
+export default Menu
---
+++ src/Components/NewsItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewsItem() {
+  return (
+    <div>
+      Item
+    </div>
+  )
+}
+
+export default NewsItem
