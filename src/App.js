8a2c26ebe6e568881c449465e976d27c46637388
rodrigomenezes22 <69732792+rodrigomenezes22@users.noreply.github.com>
2023-02-03 11:46:16 +0100

    Especial Effects

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { flushSync } from 'react-dom';
 import { BASE_API_URL } from './utils/constants';
 import { SEARCH_QUERY } from './utils/constants';
 import { DATE_SEARCH } from './utils/constants';
 import { FRONT_PAGE } from './utils/constants';
 import { isCompositeComponent } from 'react-dom/test-utils';
 import Menu from './Components/Menu';
+import gifimage from './imgs/loading.gif';
 
 import NewsItem from './Components/NewsItem';
 
@@ -25,8 +27,9 @@ useEffect(() => {
   axios
   .get(BASE_API_URL + SEARCH_QUERY + FRONT_PAGE)
   .then(res => {
+    setIsLoading(false);
      return setNews(res.data.hits)
-     setIsLoading(false);
+
   })
   .catch((error) => {
       console.log(error)
@@ -34,7 +37,9 @@ useEffect(() => {
 
 }, []);
 
-
+useEffect(() => {
+  setSearchTerm(searchTerm);
+}, [searchTerm]);
 
 const handleSearch = () => {
   setIsLoading(true);
@@ -42,9 +47,9 @@ const handleSearch = () => {
   axios
   .get(`http://hn.algolia.com/api/v1/search?query=${searchTerm}`)
   .then(res => {
-
+    setIsLoading(false);
      return setNews(res.data.hits, ...news)
-     setIsLoading(false);
+
   })
   .catch((error) => {
       console.log(error)
@@ -52,8 +57,12 @@ const handleSearch = () => {
 }
 
 const homeLogo = () => {
-  setSearchTerm('front_page');
-  handleSearch();
+
+
+    setSearchTerm('front_page'); 
+
+ 
+    handleSearch();
 }
 
 
@@ -63,9 +72,9 @@ const pastNews = () => {
   axios
   .get(`http://hn.algolia.com/api/v1/search_by_date?tags=story`)
   .then(res => {
-
+    setIsLoading(false);
      return setNews(res.data.hits, ...news)
-     setIsLoading(false);
+
   })
   .catch((error) => {
       console.log(error)
@@ -78,9 +87,9 @@ const commentsGet = () => {
   axios
   .get(`http://hn.algolia.com/api/v1/search?query=bar&tags=comment`)
   .then(res => {
-
+    setIsLoading(false);
      return setNews(res.data.hits, ...news)
-     setIsLoading(false);
+
   })
   .catch((error) => {
       console.log(error)
@@ -99,25 +108,32 @@ const showGet = () => {
 
 const handleSubmit = (event) => {
   event.preventDefault();
+
   setSearchTerm(event.target.search.value);
+
   handleSearch();
   event.target.search.value = '';
 
 };
 
 
-
-
-
   return (
     <div className="App">
-        <Menu homeLogo={homeLogo} pastNews={pastNews} commentsGet={commentsGet} jobGet={jobGet} showGet={showGet} handleSubmit={handleSubmit} />
-
+        <Menu homeLogo={homeLogo} pastNews={pastNews} commentsGet={commentsGet} jobGet={jobGet} showGet={showGet} handleSubmit={handleSubmit} setSearchTerm={setSearchTerm} />
+        {
+          isLoading ? 
+          <div className='loading_wrapper'>
+            <div className='loading_image'>
+              <img src={gifimage} alt="Loading Image" />
+            </div>
+          </div>
+          : <></>
+        }
 
         {
           news.map(article=>(
             <div>
-            <NewsItem id={article.points} title={article.title}  username={article.author} url={article.url} time={article.created_at_i} comments={ article.num_comments } />
+            <NewsItem id={article.points} title={article.title}  username={article.author} url={article.url} time={article.created_at} comments={ article.num_comments } />
             </div>
             )
           )
